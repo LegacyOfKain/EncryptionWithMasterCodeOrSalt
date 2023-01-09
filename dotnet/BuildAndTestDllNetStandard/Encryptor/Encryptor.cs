65212fc2662c913e86fdd4bb891718c38847b8c6
biswapr2 <priyanku.biswas@novartis.com>
2023-01-09 18:03:55 +0100

    Finished with dotnet project

---
+++ dotnet/BuildAndTestDllNetStandard/Encryptor/Encryptor.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Security.Cryptography;
+using System.IO;
+using Encryptor.Properties;
+using System.Xml.Linq;
+
+namespace Encryption
+{
+    public class Encryptor
+    {
+        private byte[] key = null;
+        private byte[] iv = null;
+
+        public Encryptor()
+        { 
+        }
+
+        public string EncryptPassword(string password2Encrypt, string masterName)
+        {
+            byte[] salt = Encoding.Default.GetBytes(masterName);
+            CreateKeyAndIV(Resources.EncryptPassword, salt);
+            byte[] encryptedBytes = Encrypt(password2Encrypt);
+            //return Encoding.Default.GetString(encryptedBytes, 0, encryptedBytes.Count());
+            return ByteArrayToString(encryptedBytes);
+        }
+
+        public string DecryptPassword(string encryptedPasswordString, string masterName)
+        {
+            byte[] encryptedPassword = //Encoding.Default.GetBytes(encryptedPasswordString);
+                StringToByteArray(encryptedPasswordString);
+            byte[] salt = Encoding.Default.GetBytes(masterName);
+            CreateKeyAndIV(Resources.EncryptPassword, salt);
+            return Decrypt(encryptedPassword);
+        }
+
+        private void CreateKeyAndIV(string password, byte[] salt)
+        {
+            using (Aes aes = Aes.Create())
+            {
+                var r = new Rfc2898DeriveBytes(password, salt);
+                key = r.GetBytes(aes.KeySize / 8);
+                iv = r.GetBytes(aes.BlockSize / 8);
+            }
+        }
+
+        private byte[] Encrypt(string plainText)
+        {
+            byte[] encrypted = null;
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = key;
+                aes.IV = iv;
+                ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter w = new StreamWriter(cs))
+                        {
+                            w.Write(plainText);
+                        }
+                        encrypted = ms.ToArray();
+                    }
+                }
+            }
+            return encrypted;
+        }
+
+        private string Decrypt(byte[] encryptedBytes)
+        {
+            string plainText = null;
+            using (Aes aes = Aes.Create())
+            {
+                aes.IV = iv;
+                aes.Key = key;
+                ICryptoTransform encryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+                using (MemoryStream ms = new MemoryStream(encryptedBytes))
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader sr = new StreamReader(cs))
+                        {
+                            plainText = sr.ReadToEnd();
+                        }
+                    }
+                }
+            }
+            return plainText;
+        }
+
+        private string ByteArrayToString(byte[] bytes)
+        {
+            return System.Convert.ToBase64String(bytes);
+        }
+
+        private byte[] StringToByteArray(string byteString)
+        {
+            return System.Convert.FromBase64String(byteString);
+        }
+    }
+}
---
+++ dotnet/BuildAndTestDllNetStandard/TestDllFromNet7/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Diagnostics;
+
+Debug.WriteLine("Testing .NET Standard 2.0 DLL from .NET 7 Console App !");
+
+var masterCode = @"{2A517BA1-9DA5-4259-AE1D-711B2571B415}";
+var enc = new Encryption.Encryptor();
+var encryptedPassword = enc.EncryptPassword("jk", masterCode);
+
+Debug.WriteLine("encryptedPassword = " + encryptedPassword);
+
+var decryptedPassword = enc.DecryptPassword(@"QSqhu8o/t8MFRA0XNjGrfg==", masterCode);
+Debug.WriteLine("decryptedPassword = " + decryptedPassword);
+
